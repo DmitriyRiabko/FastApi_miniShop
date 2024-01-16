9d413f35d19b7eb62fbff0687f855253b98e73b1
vsegda_vinovat <riabko97@gmail.com>
2024-01-17 01:17:56 +0200

    created session dependency

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -1,5 +1,12 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+    AsyncSession,
+)
 from sqlalchemy.orm import sessionmaker
+
+from asyncio import current_task
 
 from core.config import settings
 
@@ -11,6 +18,17 @@ class DataBaseHelper:
         self.session_factory = async_sessionmaker(
             bind=self.engine, autoflush=False, autocommit=False, expire_on_commit=False
         )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory, scopefunc=current_task
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.get_scoped_session() as session:
+            yield session
+            await session.remove()
 
 
 db_helper = DataBaseHelper(url=settings.db_url, echo=settings.db_echo)
