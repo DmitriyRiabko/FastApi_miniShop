5dd8cc4c18a59cfe756896dde1556279a23e103b
vsegda_vinovat <riabko97@gmail.com>
2024-01-18 13:43:25 +0200

    added new router

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, Path
 from typing import Annotated
 
-
+from core.config import settings
 from core.models import Base, db_helper
 from fastapi.concurrency import asynccontextmanager
 from items_views import router as items_router
 from users.views import router as users_router
+from api_v1 import router as router_v1
 
 
 @asynccontextmanager
@@ -16,6 +17,8 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="FastAPI_try_2", lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+
 app.include_router(items_router, tags=["Items"])
 app.include_router(users_router, tags=["Users"])
 
