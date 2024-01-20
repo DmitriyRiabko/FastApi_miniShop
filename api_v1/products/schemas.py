ee1ae07ffbb81980d883daac3381b360f3323428
vsegda_vinovat <riabko97@gmail.com>
2024-01-20 19:26:27 +0200

    added new schemas

--- api_v1/products/schemas.py
+++ api_v1/products/schemas.py
@@ -1,19 +1,26 @@
 from pydantic import BaseModel, ConfigDict
 
 
-
 class ProductBase(BaseModel):
-   
-   name:str
-   description: str
-   price: int
-    
-    
+    name: str
+    description: str
+    price: int
+
+
 class ProductCreate(ProductBase):
     ...
-    
-    
-    
-class Product (ProductBase):
-    model_config = ConfigDict(from_attributes = True)
-    id:int
+
+
+class ProductUpdate(ProductBase):
+    ...
+
+
+class ProductUpdatePartial(ProductBase):
+    name: str | None  = None
+    description: str | None = None
+    price: int | None = None
+
+
+class Product(ProductBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
