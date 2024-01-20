71b1e78b9c8388b96c54dd811d151401b6d8f895
vsegda_vinovat <riabko97@gmail.com>
2024-01-21 00:12:16 +0200

    added user model for export

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ("Base", "db_helper", "DataBaseHelper", "Product")
+__all__ = ("Base", "db_helper", "DataBaseHelper", "Product","User")
 
 
 from .base import Base
 from .product import Product
+from .user import User
 from .db_helper import db_helper, DataBaseHelper
