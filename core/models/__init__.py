d29c484e361e6a1684fe24852a8bf3b4acfe7838
vsegda_vinovat <riabko97@gmail.com>
2024-01-21 19:36:47 +0200

    added import

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,8 +1,9 @@
-__all__ = ("Base", "db_helper", "DataBaseHelper", "Product","User","Post")
+__all__ = ("Base", "db_helper", "DataBaseHelper", "Product", "User", "Post","Profile")
 
 
 from .base import Base
 from .product import Product
 from .user import User
 from .post import Post
+from .profile import Profile
 from .db_helper import db_helper, DataBaseHelper
