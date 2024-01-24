2952849dea5453a7637d6a023a61d48b159f525a
vsegda_vinovat <riabko97@gmail.com>
2024-01-24 16:26:35 +0200

    added some querys

--- crud.py
+++ crud.py
@@ -1,25 +1,66 @@
 import asyncio
-from core.models import db_helper, User, Profile, Post
+from core.models import db_helper, User, Profile
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.orm import joinedload
 
-async def create_user(session:AsyncSession, username:str) -> User:
+
+async def create_user(session: AsyncSession, username: str) -> User:
     user = User(username=username)
     session.add(user)
     await session.commit()
-    print(f'user {user}')
+    print(f"user {user}")
     return user
-    
-    
-    
 
 
+async def get_user_by_username(session: AsyncSession, username: str) -> User | None:
+    stmt = select(User).where(User.username == username)
+    result: Result = await session.execute(stmt)
+    user: User | None = result.scalar_one_or_none()
+    print(f"found user {username}, {user}")
+    return user
+
+
+async def create_user_profile(
+    session: AsyncSession,
+    user_id: int,
+    first_name: str | None = None,
+    last_name: str | None = None,
+) -> Profile:
+    profile = Profile(user_id=user_id, first_name=first_name, last_name=last_name)
+
+    session.add(profile)
+    await session.commit()
+    return profile
+
+
+async def show_users_with_profile(session: AsyncSession) -> list[User]:
+    stmt = select(User).options(joinedload(User.profile)).order_by(User.id)
+    # result: Result = await session.execute(stmt)
+    users = await session.scalars(stmt)
+    for user in users:
+        print(user)
+        print(user.profile.first_name)
 
 
 async def main():
     async with db_helper.session_factory() as session:
-        await create_user(session=session,username='john')
-        await create_user(session=session,username='sam')
-        
+        # await create_user(session=session,username='john')
+        # await create_user(session=session,username='sam')
 
-if __name__ == '__main__':
+        # user_sam = await get_user_by_username(session=session, username="sam")
+        # user_john = await get_user_by_username(session=session, username="john")
+        # await create_user_profile(
+        #     session=session, user_id=user_john.id, first_name="John"
+        # )
+
+        # await create_user_profile(
+        #     session=session, user_id=user_sam.id, first_name="Sam",last_name='White'
+        # )
+        await show_users_with_profile(session=session)
+        ...
+
+
+if __name__ == "__main__":
     asyncio.run(main())
