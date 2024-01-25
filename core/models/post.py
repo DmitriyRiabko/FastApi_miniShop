2329a52f5a2ec87edab8064de47e77eed510ba4e
vsegda_vinovat <riabko97@gmail.com>
2024-01-25 23:30:59 +0200

    addded __repr__

--- core/models/post.py
+++ core/models/post.py
@@ -14,3 +14,11 @@ class Post(UserRelationMixin,Base):
     title: Mapped[str] = mapped_column(String(100))
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
    
+   
+   
+    def __str__(self) -> str:
+        return f'{self.__class__.__name__}(id={self.id}, title={self.title!r})'
+    
+    
+    def __repr__(self) -> str:
+        return str(self)
