5c3733533dd075e69555f649237aa18d436ec7b3
vsegda_vinovat <riabko97@gmail.com>
2024-01-26 16:18:49 +0200

    added order to imports

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,13 @@
-__all__ = ("Base", "db_helper", "DataBaseHelper", "Product", "User", "Post","Profile")
+__all__ = (
+    "Base",
+    "db_helper",
+    "DataBaseHelper",
+    "Product",
+    "User",
+    "Post",
+    "Profile",
+    "Order",
+)
 
 
 from .base import Base
@@ -6,4 +15,5 @@ from .product import Product
 from .user import User
 from .post import Post
 from .profile import Profile
+from .order import Order
 from .db_helper import db_helper, DataBaseHelper
