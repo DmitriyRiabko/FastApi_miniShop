9b00d3c315b3a65df240734173d99f3d3b11f0d2
vsegda_vinovat <riabko97@gmail.com>
2024-01-26 16:18:41 +0200

    added order model

---
+++ core/models/order.py
@@ -0,0 +1,15 @@
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+from .base import Base
+
+class Order(Base):
+   __tablename__ = 'orders'
+   
+   promocode: Mapped[str]
+   created_at: Mapped[datetime] = mapped_column(
+       server_default=func.now(),
+       default=datetime.utcnow)
+   
+    
+    
