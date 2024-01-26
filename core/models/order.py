4e9bf63d596f0be584a08d5a5d22e600c8da95ff
vsegda_vinovat <riabko97@gmail.com>
2024-01-26 16:23:37 +0200

    fix order model

--- core/models/order.py
+++ core/models/order.py
@@ -6,7 +6,7 @@ from .base import Base
 class Order(Base):
    __tablename__ = 'orders'
    
-   promocode: Mapped[str]
+   promocode: Mapped[str | None]
    created_at: Mapped[datetime] = mapped_column(
        server_default=func.now(),
        default=datetime.utcnow)
