04ac38e4db8133f753e6647afdecf130dd436bd1
vsegda_vinovat <riabko97@gmail.com>
2024-01-27 18:45:41 +0200

    table fix

--- core/models/order_product_association.py
+++ core/models/order_product_association.py
@@ -9,8 +9,8 @@ order_product_association_table = Table(
     Base.metadata,
     Column('id',Integer, primary_key=True),
     
-    Column('order_id', ForeignKey('orders.id')),
-    Column('product_id', ForeignKey('products.id')),
-    UniqueConstraint('order_id','product_id'),
+    Column('order_id', ForeignKey('orders.id'), nullable=False),
+    Column('product_id', ForeignKey('products.id'), nullable=False),
+    UniqueConstraint('order_id','product_id', name='idx_unique_order_product'),
     
 )
