044906a7d4cafcab87c7c667808faa89426601d4
vsegda_vinovat <riabko97@gmail.com>
2024-01-31 23:08:38 +0200

    added imports

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -7,7 +7,7 @@ __all__ = (
     "Post",
     "Profile",
     "Order",
-    "order_product_association_table",
+    "OrderProductAssociation",
 )
 
 
@@ -17,6 +17,5 @@ from .user import User
 from .post import Post
 from .profile import Profile
 from .order import Order
-from .order_product_association import order_product_association_table
+from .order_product_association import OrderProductAssociation
 from .db_helper import db_helper, DataBaseHelper
-
